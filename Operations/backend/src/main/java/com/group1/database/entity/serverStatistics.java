d9ad6a848d51c214ba6ae720a9b5e12fa5416d6d
sriku1993 <sriku1993@gmail.com>
2016-10-26 19:05:46 +0200

    The initial empty framework for our application

---
+++ Operations/backend/src/main/java/com/group1/database/entity/serverStatistics.java
@@ -0,0 +1,15 @@
+package com.group1.database.entity;
+
+/**
+ * Created by sriku on 2016-10-26.
+ */
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.NotEmpty;
+
+public class serverStatistics {
+
+
+
+}
