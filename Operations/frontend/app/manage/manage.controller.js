11ef4d7ba016f75edbeb8a01e21206b6e0ccccd7
XuXiaoji <ninethousandyears@outlook.com>
2016-11-30 12:08:34 +0100

    Manage server page in the frontend

--- Operations/frontend/app/manage/manage.controller.js
+++ Operations/frontend/app/manage/manage.controller.js
@@ -4,5 +4,23 @@
 angular.module('app')
 
     .controller('ManageController', function($scope, $http, cpuUsageService, diskUsageService, memoryUsageService) {
+        var metrics;
+        var Time = [];
+        var timemax;
 
-    });
+
+
+
+        cpuUsageService.list()
+            .then(function (response) {
+                metrics = response.data;
+
+                $scope.hostnames = Array.from(new Set(metrics.map(function (metric) {
+                    return metric.hostName;
+                })));
+
+                Time = metrics.map(function(metric){
+                    return metric.time;
+                });
+                $scope.timemax = Time.slice(-1);
+    })});
--- Operations/frontend/app/manage/manage.tpl
+++ Operations/frontend/app/manage/manage.tpl
@@ -1,3 +1,20 @@
 <div ng-controller="ManageController">
-hiiiiii
+<h1>CPU Usage</h1>
+  <table>
+      <tr>
+          <th>NO.</th>
+          <th>Hostname</th>
+          <th>Lastest upload date</th>
+          <th>Controller</th>
+      </tr>
+    <tr ng-repeat="hostname in hostnames">
+        <td>{{ $index + 1 }}</td>
+        <td>{{hostname}}</td>
+        <td ng-repeat="x in timemax"> {{x | date :"yyyy-MM-dd HH:mm"}}</td>
+        <td> <button class="btn btn-danger">Delete</button></td>
+    </tr>
+  </table>
+
+
+    </div>
 </div>
